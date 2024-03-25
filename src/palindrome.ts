57c41e40ca471c075a098b9ccc02be86da8d2d43
Fabrice-Dush <dushimimanafabricerwanda@gmail.com>
2024-03-25 12:03:28 +0300

    Initial commit

--- src/palindrome.ts
+++ src/palindrome.ts
@@ -1,7 +1,14 @@
 const isPalindrome = function (str: string): boolean {
-  const temp: string = str;
-  str = str.split('').reverse().join('');
+  //prettier-ignore
+  const exceptions: string[] = ["?", "!", ".", "(", ")", ",", "/",":", " ", ";", "'"];
+  exceptions.forEach(exception => {
+    str = str.replaceAll(exception, '');
+  });
+  const temp: string = str.toLowerCase();
+  str = str.toLowerCase().split('').reverse().join('');
   return str === temp;
 };
 console.log(isPalindrome('aba'));
 console.log(isPalindrome('fab'));
+console.log(isPalindrome('radar'));
+console.log(isPalindrome('Was it a car or a cat I saw?'));
--- src/prime.ts
+++ src/prime.ts
@@ -12,4 +12,5 @@ const isPrime = function (arr: Numbers): Numbers {
 
   return primeArr;
 };
-console.log(isPrime([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+// console.log(isPrime([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+console.log(isPrime([356, 701, 39, 46, 133]));
