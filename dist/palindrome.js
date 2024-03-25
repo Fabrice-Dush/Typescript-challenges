010bbc113d0b036d9f92fe560da9de9333dafcb8
Fabrice-Dush <dushimimanafabricerwanda@gmail.com>
2024-03-25 12:06:44 +0300

    Initial commit

--- dist/palindrome.js
+++ dist/palindrome.js
@@ -1,8 +1,15 @@
 "use strict";
 const isPalindrome = function (str) {
-    const temp = str;
-    str = str.split('').reverse().join('');
+    //prettier-ignore
+    const exceptions = ["?", "!", ".", "(", ")", ",", "/", ":", " ", ";", "'"];
+    exceptions.forEach(exception => {
+        str = str.replaceAll(exception, '');
+    });
+    const temp = str.toLowerCase();
+    str = str.toLowerCase().split('').reverse().join('');
     return str === temp;
 };
 console.log(isPalindrome('aba'));
 console.log(isPalindrome('fab'));
+console.log(isPalindrome('radar'));
+console.log(isPalindrome('Was it a car or a cat I saw?'));
